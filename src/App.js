c2ca8837c11229d8ab7f1c293ae67df7cf9c6b10
unknown <girisagar0014@gmail.com>
2021-08-03 19:25:25 +0545

    filter section for products

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Card } from "./components/card";
 import { Navbar } from "./components/navbar";
+import { Product } from "./components/product";
 import { productsData } from "./productsData";
 
 const Wrapper = styled.div``;
@@ -22,21 +23,59 @@ const ProductList = styled.div`
   gap: 30px;
   align-items: center;
 `;
+const FilterSection = styled.div`
+  width: 125px;
+
+  > select {
+    background: #2c3e50;
+    color: #e8e4e4;
+    text-transform: capitalize;
+    font-size: 14px;
+    padding: 4px 17px;
+    border: none;
+    outline: none;
+  }
+`;
 function App() {
-  const [items, setItems] = useState(null)
+  const [filter, setFilter] = useState("All");
+  const [items, setItems] = useState(null);
+
+  //fetch and filter data if needed
   useEffect(() => {
     if (productsData) {
-      setItems(productsData);
+      if (filter !== "All") {
+        let filteredData;
+        filteredData = productsData.filter(
+          (product) => product.category === filter
+        );
+        setItems(filteredData);
+      } else {
+        setItems(productsData);
+      }
     }
-  }, [productsData]);
+  }, [productsData, filter]);
+
   return (
     <Wrapper>
       <Navbar />
+
       <Content>
+        <Card style={{ maxWidth: "130px" }}>
+          <Header>Filter</Header>
+          <FilterSection>
+            <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+              <option value="All">All</option>
+              <option value="Fruits">Fruits</option>
+              <option value="Vegetables">Vegetable</option>
+            </select>
+          </FilterSection>
+        </Card>
         <Card>
           <Header>Groceries</Header>
           <ProductList>
-           </ProductList>
+            {items &&
+              items.map((item) => <Product key={item.id} item={item} />)}
+          </ProductList>
         </Card>
       </Content>
     </Wrapper>
